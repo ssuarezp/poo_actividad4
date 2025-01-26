763e787673d6078acb35c82eb8e80c3361c879b8
ssuarezp <ssuarezp@unal.edu.co>
2025-01-25 19:39:37 -0500

    2

--- actividad4_ejercicio4.8/src/CarreraCiclistica/Contrarrelojista.java
+++ actividad4_ejercicio4.8/src/CarreraCiclistica/Contrarrelojista.java
@@ -8,6 +8,30 @@ package CarreraCiclistica;
  *
  * @author EstudianteAuxiliar
  */
-public class Contrarrelojista {
+public class Contrarrelojista extends Ciclista {
+    
+    private double velocidadMaxima;
+    
+    public Contrarrelojista(int identificador, String nombre, double velocidadMáxima) {
+        super(identificador, nombre);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    protected double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+    
+    protected void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    protected void imprimir() {
+        super.imprimir(); // Invoca el método imprimir de la clase padre
+        System.out.println("Aceleración promedio = " + velocidadMaxima);
+    }
+    
+    protected String imprimirTipo() {
+        return "Es un constrarrelojista";
+    }
     
 }
--- actividad4_ejercicio4.8/src/CarreraCiclistica/Escalador.java
+++ actividad4_ejercicio4.8/src/CarreraCiclistica/Escalador.java
@@ -8,6 +8,41 @@ package CarreraCiclistica;
  *
  * @author EstudianteAuxiliar
  */
-public class Escalador {
+public class Escalador extends Ciclista {
+    private double aceleracionPromedio;
+    private double gradoRampa;
     
+    public Escalador(int identificador, String nombre, double 
+        aceleracionPromedio, double gradoRampa) {
+        super(identificador, nombre);
+        this.aceleracionPromedio = aceleracionPromedio;
+        this.gradoRampa = gradoRampa;
+    }
+    
+    protected double getAceleracionPromedio() {
+        return aceleracionPromedio;
+    }
+    
+    protected void setAceleracionPromedio(double aceleracionPromedio) {
+    this.aceleracionPromedio = aceleracionPromedio;
+    }
+    
+    protected double getGradoRampa() {
+        return gradoRampa;
+    }
+    
+    protected void setGradoRampa(double gradoRampa) {
+        this.gradoRampa = gradoRampa;
+    }
+    
+    protected void imprimir() {
+        super.imprimir(); // Invoca el método imprimir de la clase padre
+        System.out.println("Aceleración promedio = " + aceleracionPromedio);
+        System.out.println("Grado de rampa = " + gradoRampa);
+    }
+    
+    protected String imprimirTipo() {
+        return "Es un escalador";
+    }
+
 }
